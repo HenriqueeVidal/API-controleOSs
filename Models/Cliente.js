2593d0cf01e3c536bee003e5f7a7953ff1ed203a
Marcelo <27177081+Smart624@users.noreply.github.com>
2023-05-06 23:53:04 -0300

    Feito os models dos objetos

--- Models/Cliente.js
+++ Models/Cliente.js
@@ -15,10 +15,9 @@ Atributos:
 •	Bairro - texto
 •	Cidade - texto
 •	UF - texto
-
  */
 
-class Cliente{
+export class Cliente{
     constructor(id, cpfCnpj, nome, telefone, email, cep, endereco, numero, complemento, bairro, cidade, uf){
         this.id = id;
         this.cpfCnpj = cpfCnpj;
@@ -35,3 +34,51 @@ class Cliente{
     }
 }
 
+export const getAll = () => {
+    return dbClientes;
+}
+
+export const dbClientes = [
+    new Cliente(1, 123456789, "João da Silva", 123456789, "cliente@gmail.com", 123456789, "Rua dos Bobos", 0, "Casa", "Centro", "São Paulo", "SP"),
+        new Cliente(2, 123456789, "Maria da Silva", 123456789, "maria@gmail.com", 123456789, "Rua dos Bobos", 0, "Casa", "Centro", "São Paulo", "SP"),
+            ]
+
+
+export const getById = (id) => {
+    return dbClientes.find(cliente => cliente.id === parseInt(id))
+}
+
+export const create = (clienteData) => {
+    const newCliente = new Cliente(
+        Math.max(...dbClientes.map(cliente => cliente.id)) + 1,
+        clienteData.cpfCnpj,
+        clienteData.nome,
+        clienteData.telefone,
+        clienteData.email,
+        clienteData.cep,
+        clienteData.endereco,
+        clienteData.numero,
+        clienteData.complemento,
+        clienteData.bairro,
+        clienteData.cidade,
+        clienteData.uf
+    )
+    dbClientes.push(newCliente)
+    return newCliente
+}
+
+export const update = (id, updatedClienteData) => {
+    const clienteIndex = dbClientes.findIndex(cliente => cliente.id === parseInt(id))
+    if (clienteIndex !== -1) {
+        Object.assign(dbClientes[clienteIndex], updatedClienteData)
+        return dbClientes[clienteIndex]
+    }
+    return null
+}
+
+export const remove = (id) => {
+    const clienteIndex = dbClientes.findIndex(cliente => cliente.id === parseInt(id))
+    if (clienteIndex !== -1) {
+        dbClientes.splice(clienteIndex, 1)
+    }
+}
