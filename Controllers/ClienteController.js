8bac319c0e6aebd79797ce448071634587b5c14d
Marcelo <27177081+Smart624@users.noreply.github.com>
2023-05-15 16:15:10 -0300

    Updated ClienteController, OrdemDeServicoController, Cliente, IrdenDeServico in the models package, ClienteRoutes, OrdemDeServicoRoutes and app, providing the CRUD function

--- Controllers/ClienteController.js
+++ Controllers/ClienteController.js
@@ -1,28 +1,91 @@
-import { getAll, getById, create, update, remove } from "../Models/Cliente.js";
+// Import the necessary functions and classes from the models
+import { Cliente, create, destroy, findAll, findByPk, update } from "../models/Cliente.js"
 
+// Define a controller for handling operations related to 'Cliente'
 class ClienteController {
-    static list(req, res) {
-        res.json(getAll())
+    // Handles a request to get a list of all 'Cliente' records
+    static list(req,res){
+        res.json(findAll())
     }
 
-    static find(req, res) {
-        res.json(getById(req.params.id))
+    // Handles a request to get a 'Cliente' record by ID
+    static getClienteById(req,res){
+        const id = parseInt(req.params.id) // Parse the ID from the request parameters
+        const cliente = findByPk(id) // Find the 'Cliente' by primary key
+        if(!cliente){
+            res.status(404).json({error:"Não encontrado"}) // If no 'Cliente' is found, send a 404 response
+            return
+        }
+
+        res.json(cliente) // If 'Cliente' is found, send it in the response
     }
 
-    static create(req, res) {
-        const newCliente = create(req.body) // O que é body?
-        res.status(201).json(newCliente)
+    // Handles a request to delete a 'Cliente' record by ID
+    static destroyCliente(req,res){
+        const id = parseInt(req.params.id) // Parse the ID from the request parameters
+        const cliente = findByPk(id) // Find the 'Cliente' by primary key
+        if(!cliente){
+            res.status(404).json({error:"Não encontrado"}) // If no 'Cliente' is found, send a 404 response
+            return
+        }
+        destroy(id) // Destroy the 'Cliente' record
+        res.json({message: "Removido com sucesso!"}) // Send a success message in the response
     }
 
-    static update(req, res) {
-        const updatedCliente = update(req.params.id, req.body) // O que é body?
-        res.json(updatedCliente)
+    // Handles a request to create a new 'Cliente' record
+    static createCliente(req,res){
+        // Destructure the data from the request body
+        const {cpf_cnpj, nome, telefone, celular, email, cep, endereco, numero, complemento, bairro, cidade, uf} = req.body
+
+        // Check if all necessary fields are present
+        if(!cpf_cnpj || !nome || !celular || !email || !cep || !endereco || !numero || !bairro || !cidade || !uf){
+            res.status(400).json({error: "Informe todos os campos!"}) // If not, send a 400 response
+            return
+        }
+
+        // Create a new 'Cliente' instance
+        const cliente = new Cliente(0, cpf_cnpj, nome, telefone, celular, email, cep, endereco, numero, complemento, bairro, cidade, uf)
+        const createdCliente = create(cliente) // Add the new 'Cliente' to the database
+        res.status(201).json(createdCliente) // Send the created 'Cliente' in the response
     }
 
-    static delete(req, res) {
-        remove(req.params.id)
-        res.status(204).end()
+    // Handles a request to update a 'Cliente' record by ID
+    static updateCliente(req,res){
+        const id = parseInt(req.params.id) // Parse the ID from the request parameters
+        const cliente = findByPk(id) // Find the 'Cliente' by primary key
+        if(!cliente){
+            res.status(404).json({error:"Não encontrado"}) // If no 'Cliente' is found, send a 404 response
+            return
+        }
+
+        // Destructure the data from the request body
+        const {cpf_cnpj, nome, telefone, celular, email, cep, endereco, numero, complemento, bairro, cidade, uf} = req.body
+
+        // Check if all necessary fields are present
+        if(!cpf_cnpj || !nome || !celular || !email || !cep || !endereco || !numero || !bairro || !cidade || !uf){
+            res.status(400).json({error: "Informe todos os campos!"}) // If not, send a 400 response
+            return
+        }
+
+        // Update the 'Cliente' record
+        cliente.cpf_cnpj = cpf_cnpj
+        cliente.nome = nome
+        cliente.telefone = telefone
+        cliente.celular = celular
+        cliente.email = email
+        cliente.cep = cep
+        cliente.endereco = endereco
+        cliente.numero = numero
+        cliente.complemento = complemento
+        cliente.bairro = bairro
+        cliente.cidade = cidade
+        cliente.uf = uf
+
+        // Send the updated 'Cliente' record in the response
+        update(id,cliente)
+        res.json(cliente)
     }
 }
 
+// Export the controller
 export default ClienteController
--- Models/OrdemDeServico.js
+++ Models/OrdemDeServico.js
@@ -15,30 +15,73 @@ Atributos:
 •	Anexos - file
  */
 
+// models/OrdemDeServico.js
+
+// Class representing a Service Order
 export class OrdemDeServico {
-    constructor(id, cliente_id, produto_id, data_abertura, data_pronto, data_saida, status, diagnostico, solucao, anexos) {
+    constructor(id, cliente_id, produto_id, data_abertura, data_pronto, data_saida, status = 'aberta', diagnostico_defeito_reclamacao, solucao, anexos){
         this.id = id;
         this.cliente_id = cliente_id;
         this.produto_id = produto_id;
         this.data_abertura = data_abertura;
         this.data_pronto = data_pronto;
         this.data_saida = data_saida;
-        this.status = status;
-        this.diagnostico = diagnostico;
+        this.status = status;  // Default value is 'aberta'
+        this.diagnostico_defeito_reclamacao = diagnostico_defeito_reclamacao;
         this.solucao = solucao;
         this.anexos = anexos;
     }
 }
 
-export const getAll = () => {
-    return dbOrdensDeServico;
+// Variable to store the next ID to be used when creating a new Service Order
+let idAtual = 2
+
+// Function to create a new Service Order
+export const create = (OrdemDeServico) => {
+    idAtual++  // Increment the current ID
+    OrdemDeServico.id = idAtual  // Assign the new ID to the Service Order
+    dbOrdemDeServicos.push(OrdemDeServico)  // Add the new Service Order to the database
+    console.log("Service Order successfully registered!")
+    return OrdemDeServico
 }
 
-export const getById = (id) => {
-    return dbOrdensDeServico.find(OrdemDeServico => dbOrdensDeServico.id === parseInt(id))
+// Function to find a Service Order by its primary key
+export const findByPk = (id) => {
+    return dbOrdemDeServicos.find(OrdemDeServico => OrdemDeServico.id === id)
 }
 
-export const dbOrdensDeServico = [
-    new OrdemDeServico(1, 1, 1, "2021-01-01", "2021-01-02", "2021-01-03", "aberta", "diagnostico", "solucao", "anexos"),
-    new OrdemDeServico(2, 2, 2, "2021-02-01", "2021-02-02", "2021-02-03", "em andamento", "diagnostico", "solucao", "anexos")
+// Function to return all Service Orders
+export const findAll = () => {
+    return dbOrdemDeServicos
+}
+
+// Function to delete a Service Order
+export const destroy = (id) => {
+    const OrdemDeServico = findByPk(id)
+    if(!OrdemDeServico){
+        console.log("Service Order not found!")
+        return false
+    }
+    const index = dbOrdemDeServicos.indexOf(OrdemDeServico)
+    dbOrdemDeServicos.splice(index,1)
+    console.log("Service Order successfully removed!")
+    return true
+}
+
+// Function to update a Service Order
+export const update = (id, OrdemDeServicoUpdate) => {
+    const OrdemDeServico = findByPk(id)
+    if(!OrdemDeServico) {
+        console.log("Service Order not found!")
+        return false
+    }
+    const index = dbOrdemDeServicos.indexOf(OrdemDeServico)
+    dbOrdemDeServicos[index] = OrdemDeServicoUpdate
+    console.log("Service Order successfully updated!")
+    return true
+}
+
+// Initial Service Orders database
+export const dbOrdemDeServicos = [
+    new OrdemDeServico(1, 1, 1, '2021-05-01', '2021-05-02', '2021-05-03', 'aberta', 'Não liga', 'Troca de fonte', 'fonte.jpg')
 ]
