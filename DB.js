83d4ebfa867e3921d9a9c77599a21eaee762bab9
Marcelo <27177081+Smart624@users.noreply.github.com>
2023-05-24 00:31:33 -0300

    changed from array to SQLite database, using Sequelize for cliente, Orcamento and OrdemDeServico, added DB classe and changed app to support the database

--- DB.js
+++ DB.js
@@ -1,1 +1,21 @@
-import { Sequelize} from "sequelize";
+import { Sequelize } from "sequelize"; // Importa o módulo Sequelize
+
+// Cria uma nova instância do Sequelize para se conectar ao banco de dados
+const db = new Sequelize(
+    "database", // Nome do banco de dados
+    "user", // Nome de usuário do banco de dados
+    "passw", // Senha do banco de dados
+    {
+        dialect: "sqlite", // Define o dialeto como SQLite
+        storage: './db.sqlite' // Define o arquivo de armazenamento do banco de dados
+    }
+);
+
+export default db; // Exporta a instância do banco de dados
+
+// O código acima cria uma conexão com o banco de dados SQLite utilizando o Sequelize.
+// O banco de dados é identificado pelo nome "database" e a conexão utiliza um nome de usuário "user" e uma senha "passw".
+// O dialeto é definido como SQLite e o arquivo "db.sqlite" é especificado como o local de armazenamento do banco de dados.
+
+// A instância do Sequelize, representada pela constante "db", será utilizada para realizar operações no banco de dados,
+// como definir modelos, executar consultas, realizar migrações, entre outras funcionalidades oferecidas pelo Sequelize.
--- Models/Orcamento.js
+++ Models/Orcamento.js
@@ -1,76 +1,45 @@
 /*
-Orçamento
-Descrição: Gerencia os orçamentos associados às ordens de serviço, armazenando informações sobre o custo das peças e da mão de obra, além do status do orçamento (enviado, aprovado, recusado).
-Atributos
-•	Id - número
-•	Ordem_servico_id (relacionamento com Ordem de Serviço)
-•	Valor_peças - número
-•	Valor_mão_de_obra - número
-•	Status (enviado, aprovado, recusado) - texto
+-- Tabela Orçamento
+CREATE TABLE orcamento (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ordem_servico_id INTEGER NOT NULL,
+    valor_pecas DECIMAL(10, 2) NOT NULL,
+    valor_mao_de_obra DECIMAL(10, 2) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    FOREIGN KEY (ordem_servico_id) REFERENCES ordem_servico (id)
+);
  */
 
-// models/Orcamento.js
+import { Sequelize } from "sequelize";
+import db from "../db.js";
+import OrdemServico from "./OrdemDeServico.js";
 
-// Class representing a Budget
-export class Orcamento {
-    constructor(id, ordem_servico_id, valor_pecas, valor_mao_de_obra, status){
-        this.id = id;
-        this.ordem_servico_id = ordem_servico_id;
-        this.valor_pecas = valor_pecas;
-        this.valor_mao_de_obra = valor_mao_de_obra;
-        this.status = status;
+// Definição do modelo Orcamento
+const Orcamento = db.define('orcamento', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    ordem_servico_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    valor_pecas: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+    },
+    valor_mao_de_obra: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+    },
+    status: {
+        type: Sequelize.STRING(20),
+        allowNull: false
     }
-}
+});
 
-// Variable to store the next ID to be used when creating a new Budget
-let idAtual = 2
+// Relacionamento entre Orcamento e OrdemServico
+Orcamento.belongsTo(OrdemServico, { foreignKey: 'ordem_servico_id' }); // Orcamento pertence a uma OrdemServico
 
-// Function to create a new Budget
-export const create = (Orcamento) => {
-    idAtual++  // Increment the current ID
-    Orcamento.id = idAtual  // Assign the new ID to the Budget
-    dbOrcamentos.push(Orcamento)  // Add the new Budget to the database
-    console.log("Budget successfully registered!")
-    return Orcamento
-}
-
-// Function to find a Budget by its primary key
-export const findByPk = (id) => {
-    return dbOrcamentos.find(Orcamento => Orcamento.id === id)
-}
-
-// Function to return all Budgets
-export const findAll = () => {
-    return dbOrcamentos
-}
-
-// Function to delete a Budget
-export const destroy = (id) => {
-    const Orcamento = findByPk(id)
-    if(!Orcamento){
-        console.log("Budget not found!")
-        return false
-    }
-    const index = dbOrcamentos.indexOf(Orcamento)
-    dbOrcamentos.splice(index,1)
-    console.log("Budget successfully removed!")
-    return true
-}
-
-// Function to update a Budget
-export const update = (id, OrcamentoUpdate) => {
-    const Orcamento = findByPk(id)
-    if(!Orcamento) {
-        console.log("Budget not found!")
-        return false
-    }
-    const index = dbOrcamentos.indexOf(Orcamento)
-    dbOrcamentos[index] = OrcamentoUpdate
-    console.log("Budget successfully updated!")
-    return true
-}
-
-// Initial Budgets database
-export const dbOrcamentos = [
-    new Orcamento(1, 1, 100, 50, 'pending')
-]
+export default Orcamento;
